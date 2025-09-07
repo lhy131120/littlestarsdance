a3dda9d0e2f246a9afcd6972f95288feb88ec624
lhy <lhy131120@gmail.com>
2025-09-07 23:47:23 +0800

    update header and footer layout

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 // import "https://font.emtech.cc/emfont.js";
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-console.log('Hello world');
+document.addEventListener("DOMContentLoaded", () => {
+  // console.log("Hello world");
+  // const navbarToggler = document.querySelector(".navbar-toggler");
+  // const offcanvas = document.querySelector(".offcanvas");
+  // const navbar = document.querySelector("header .navbar");
+  // navbarToggler.addEventListener("click", (e) => {
+  //   if (offcanvas.classList.contains("show")) {
+  //     navbarToggler.classList.remove("toggled");
+  //   } else {
+  //     navbarToggler.classList.add("toggled");
+  //   }
+  // });
+});
