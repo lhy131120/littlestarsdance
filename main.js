e966a4e8ec08c3216d77a28286cd9709636a0b12
lhy <lhy131120@gmail.com>
2025-09-09 04:54:19 +0800

    update card style

--- main.js
+++ main.js
@@ -5,20 +5,19 @@ import "swiper/css/bundle";
 import "./assets/scss/all.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
-
-  if(document.querySelector("#home-hero-wrap")) {
+  if (document.querySelector("#home-hero-wrap")) {
     const swiper = new Swiper("#home-hero-wrap .swiper", {
-			loop: true,
-			autoplay: {
-				delay: 5000,
-				disableOnInteraction: true,
-			},
+      loop: true,
+      // autoplay: {
+      //   delay: 5000,
+      //   disableOnInteraction: true,
+      // },
       effect: "fade",
-			navigation: {
-				nextEl: "#home-hero-wrap .swiper .swiper-button-next",
-				prevEl: "#home-hero-wrap .swiper .swiper-button-prev",
-			},
-		});
+      navigation: {
+        nextEl: "#home-hero-wrap .swiper .swiper-button-next",
+        prevEl: "#home-hero-wrap .swiper .swiper-button-prev",
+      },
+    });
   }
 
   console.log(Swiper);
