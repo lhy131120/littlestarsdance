17cce4fa6397edbf5d702af8cdd1c080b38aed42
Ray <lhy131120@gmail.com>
2025-09-09 17:45:15 +0800

    update index page

--- main.js
+++ main.js
@@ -5,19 +5,38 @@ import "swiper/css/bundle";
 import "./assets/scss/all.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
+
   if (document.querySelector("#home-hero-wrap")) {
     const swiper = new Swiper("#home-hero-wrap .swiper", {
       loop: true,
-      // autoplay: {
-      //   delay: 5000,
-      //   disableOnInteraction: true,
-      // },
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: true,
+      },
       effect: "fade",
       navigation: {
         nextEl: "#home-hero-wrap .swiper .swiper-button-next",
         prevEl: "#home-hero-wrap .swiper .swiper-button-prev",
       },
     });
+  }
+
+  if (document.querySelector("#hot-course")) {
+    const swiper = new Swiper("#hot-course .swiper", {
+			loop: true,
+			slidesPerView: 2,
+			spaceBetween: 24,
+			breakpoints: {
+				1200: {
+					slidesPerView: 3,
+					spaceBetween: 24,
+				},
+			},
+			navigation: {
+				nextEl: "#hot-course .swiper-button-next",
+				prevEl: "#hot-course .swiper-button-prev",
+			},
+		});
   }
 
   console.log(Swiper);
