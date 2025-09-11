85232188156ad371f54bf20cb7b5d40e7c52b721
Ray <lhy131120@gmail.com>
2025-09-11 15:22:12 +0800

    home page layout & RWD finished

--- main.js
+++ main.js
@@ -48,12 +48,50 @@ document.addEventListener("DOMContentLoaded", () => {
   if (document.querySelector("#home-comments")) {
     const swipers = document.querySelectorAll("#home-comments .swiper");
 
-    swipers.forEach((swiperEl) => {
+    swipers.forEach((swiperEl, index) => {
       const swiper = new Swiper(swiperEl, {
-        slidesPerView: "auto",
-        spaceBetween: 24,
-        loop: true,
-      });
+				slidesPerView: "auto",
+				spaceBetween: 24,
+				loop: true,
+        centeredSlides: true,
+        centeredSlidesBounds: true,
+				pauseOnMouseEnter: true,
+				freeMode: {
+					enabled: true,
+					minimumVelocity: 0.05,
+					momentumRatio: 1.2,
+				},
+				autoplay: {
+					delay: 5000,
+					disableOnInteraction: false,
+					reverseDirection: index === 1 ? true : false,
+				},
+				speed: 1500,
+			});
     });
   }
+
+  if(document.querySelector("#home-past-events")) {
+    const swiper = new Swiper("#home-past-events .swiper", {
+			slidesPerView: "auto",
+			spaceBetween: 8,
+			centeredSlides: false,
+			centeredSlidesBounds: false,
+      centerInsufficientSlides: true,
+			breakpoints: {
+				992: {
+					spaceBetween: 24,
+					centeredSlides: true,
+					centeredSlidesBounds: true,
+				},
+			},
+			freeMode: {
+				enabled: true,
+				minimumVelocity: 0.05,
+				momentumRatio: 1.2,
+			},
+			speed: 1500,
+		});
+  }
+
 });
