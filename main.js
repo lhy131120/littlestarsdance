6528552832405840005c551aad78db9ec3c8db9e
lhy <lhy131120@gmail.com>
2025-09-11 08:21:15 +0800

    update

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ import "swiper/css/bundle";
 import "./assets/scss/all.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
-
   if (document.querySelector("#home-hero-wrap")) {
     const swiper = new Swiper("#home-hero-wrap .swiper", {
       loop: true,
@@ -23,33 +22,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (document.querySelector("#hot-course")) {
     const swiper = new Swiper("#hot-course .swiper", {
-			loop: true,
-			slidesPerView: 2,
-			spaceBetween: 24,
-			breakpoints: {
-				1200: {
-					slidesPerView: 3,
-					spaceBetween: 24,
-				},
-			},
-			navigation: {
-				nextEl: "#hot-course .swiper-button-next",
-				prevEl: "#hot-course .swiper-button-prev",
-			},
-		});
+      loop: true,
+      slidesPerView: 2,
+      spaceBetween: 24,
+      breakpoints: {
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 24,
+        },
+      },
+      navigation: {
+        nextEl: "#hot-course .swiper-button-next",
+        prevEl: "#hot-course .swiper-button-prev",
+      },
+    });
   }
 
-  if(document.querySelector("#home-event")) {
+  if (document.querySelector("#home-event")) {
     const swiper = new Swiper("#home-event .swiper", {
       slidesPerView: "auto",
-      spaceBetween: 24
-    })
+      spaceBetween: 24,
+    });
   }
 
-  if(document.querySelector("#home-comments")) {
+  if (document.querySelector("#home-comments")) {
     const swipers = document.querySelectorAll("#home-comments .swiper");
 
-    
+    swipers.forEach((swiperEl) => {
+      const swiper = new Swiper(swiperEl, {
+        slidesPerView: "auto",
+        spaceBetween: 24,
+        loop: true,
+      });
+    });
   }
-
 });
